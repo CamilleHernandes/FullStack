ddb049b9ce0bd5e0a6ea9d1c7cc005e01f12bdc2
CamilleHernandes <camillehernandes14@gmail.com>
2025-04-28 21:33:59 -0300

    att

--- Servidor2/servidor2.js
+++ Servidor2/servidor2.js
@@ -4,6 +4,8 @@ var http = require('http'); // inclui o módulo http
 
 var express = require('express'); // incui o modulo express
 
+var bodyParser = require("body-parser")
+
 //cria a variavel app, que vai ser por onde vamos acessar
 //os métodos / funções existentes no framework express
 var app = express();
@@ -11,6 +13,8 @@ var app = express();
 //metodo use() é utilizado para definir em qual
 //pasta estara o conteudo estatico
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
 
 var server = http.createServer(app);
 
@@ -27,5 +31,44 @@ console.log("Servidor Rodando....".rainbow)
 //Exemplos de GET e POST
 
 app.get('/inicio', function(requisicao,resposta){
-    resposta.redirect('Aula9/index.html')
+    resposta.redirect('Aula1/index.html')
 })
+
+app.post('/inicio', function(requisicao,resposta){// a variavel resposta redireciona para algum lugar
+    resposta.redirect('Aula1/index.html') //faz o botao do index redirecionar para o index da aula1
+})
+
+app.get('/cadastrar', function(requisicao,resposta){// a variavel requisicao pega as informaçoes
+   let nome = requisicao.query.nome; // variavel que pega as informacoes a partir do name que esta nos inputs
+   let email = requisicao.query.email; // query é o que aparece na barra de endereço, por isso só é usado com get
+   let senha = requisicao.query.senha;
+   let nasc = requisicao.query.nasc;
+
+   console.log(nome,email,senha,nasc);
+
+   resposta.render('resposta.ejs',
+    {mensagem: "usuario cadastrado com sucesso", usuario: nome, login: email});
+})
+
+
+app.post('/cadastrar', function(requisicao,resposta){// a variavel requisicao pega as informaçoes
+   let nome = requisicao.body.nome; // variavel que pega as informacoes a partir do name que esta nos inputs
+   let email = requisicao.body.email; // o body é usado somente com post
+   let senha = requisicao.body.senha;
+   let nasc = requisicao.body.nasc;
+
+   console.log(nome,email,senha,nasc);
+
+   resposta.render('resposta.ejs', 
+    {mensagem: "usuario cadastrado com sucesso", usuario: nome, login: email}); //renderiza as informações de acordo com as variaveis colocadas no resposta.ejs
+})
+
+app.get('/for_ejs', function(requisicao,resposta){// a variavel requisicao pega as informaçoes
+    let num = requisicao.query.num
+ 
+    
+    resposta.render('exemplo_for.ejs', {tamanho: num});
+ })
+ 
+
+
